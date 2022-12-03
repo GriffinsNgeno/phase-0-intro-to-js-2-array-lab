671c6f324a5944d05d807c4ea2c8b38805bb6424
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2022-12-03 22:03:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Write your solution here
 const cats= ["Milo", "Otis", "Garfield"];
-// appends a cat to the end of the cats array
 
+// appends a cat to the end of the cats array
 function destructivelyAppendCat(name){
 cats.push(name);
 return cats;
 }
 //  prepends a cat to the beginning of the cats array
-
 function destructivelyPrependCat(name){
     return cats.unshift(name);
 }
@@ -20,24 +19,19 @@ function destructivelyPrependCat(name){
       return  cats.shift(name);
      }
 // appends a cat to the cats array and returns a new array, leaving the cats array unchanged
-    
 function appendCat(name){
        return [...cats,name];
      
      }
-// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
-    
+// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged   
 function prependCat(name){
         return [name,...cats];
      }
 //removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
-  
 function removeLastCat(){
          return cats.slice(0,-1);
-       
      }
 //removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
-
      function removeFirstCat(){
         return cats.slice(1);
       }
